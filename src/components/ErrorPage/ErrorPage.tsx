dc88dfbafd497bfa5f6c47d6e0032f1382a874a0
Wiola P <wiol@onet.eu>
2022-07-22 00:10:00 +0200

    add ErrorPage comp.

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import './ErrorPage.scss';
+
+//nawigacja 'dostępni kursanci' i 'do rozmowy'
+
+
+interface Props {
+
+}
+
+
+const ErrorPage: React.FC<Props> = () => {
+
+
+    return (
+        <main></main>
+    )
+}
+
+export default ErrorPage;
