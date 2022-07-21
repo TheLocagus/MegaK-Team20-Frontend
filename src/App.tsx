22622099ed3ce207389bd70324bbfcd696040406
Wiola P <wiol@onet.eu>
2022-07-22 00:03:42 +0200

    [FE-7] add pages routes + login page component create & style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import LoginPage from './components/LoginPage/LoginPage';
+import CandidatesListPage from './components/CandidatesListPage/CandidatesListPage';
+import CandidatePage from './components/CandidatePage/CandidatePage';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
+import Footer from './components/Footer/Footer';
 
 import './App.scss';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
@@ -7,9 +14,14 @@ export const App = () => {
 
   return (
       <BrowserRouter>
+      <Header />
         <Routes>
-          {/*<Route path={'/'} element={<CandidatesListPage/>}/>*/}
+          <Route path='/' element={ <LoginPage />}></Route>
+          <Route path='/hr-panel' element={ <CandidatesListPage />}></Route>
+          <Route path='/success' element={ <CandidatePage />}></Route>
+          <Route path='*' element={ <ErrorPage />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
   );
 }
