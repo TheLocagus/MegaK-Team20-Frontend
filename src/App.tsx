a2a045b802a81e05fa418ca01e27d6b180356c2e
KacperPietrusiak <kacperpietrusiak.bizes@gmail.com>
2022-07-22 13:12:56 +0200

    added paths in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header/Header';
 import LoginPage from './components/LoginPage/LoginPage';
 import CandidatesListPage from './components/CandidatesListPage/CandidatesListPage';
-import CandidatePage from './components/CandidatePage/CandidatePage';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 
 import Footer from './components/Footer/Footer';
 
 import './App.scss';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Panel} from "./pages/Panel/Panel";
+import CandidatePage from "./components/CandidatePage/CandidatePage";
 
 export const App = () => {
 
@@ -16,10 +17,14 @@ export const App = () => {
       <BrowserRouter>
       <Header />
         <Routes>
-          <Route path='/' element={ <LoginPage />}></Route>
-          <Route path='/hr-panel' element={ <CandidatesListPage />}></Route>
-          <Route path='/success' element={ <CandidatePage />}></Route>
-          <Route path='*' element={ <ErrorPage />}></Route>
+          <Route path='/' element={<LoginPage/>}/>
+          <Route path='hr-panel' element={<Panel/>}>
+              <Route path={'candidates'} element={<CandidatesListPage/>}/>
+              <Route path={'meetings'} element={<CandidatesListPage/>}/>
+              <Route path={':userId'} element={<CandidatePage/>}/>
+              {/*<Route path={'profile'} element={<Profile/>}/>*/}
+          </Route>
+          <Route path='*' element={<ErrorPage/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
