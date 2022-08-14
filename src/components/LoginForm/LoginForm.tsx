e2f977f881efb4ab980752e261b0c4009c05359c
TheLocagus <locagus@o2.pl>
2022-08-15 00:30:12 +0200

    Logic in Login form.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,54 +1,103 @@
 import ButtonLink from 'components/common/ButtonLink/ButtonLink';
-import { labels } from 'utils/labels'
+import {labels} from 'utils/labels'
 
 import './LoginForm.scss';
+import {SyntheticEvent, useState} from "react";
 
 
 interface Props {
-    onClick?: () => void;
+  onClick?: () => void;
 }
 
 
-const LoginForm: React.FC<Props> = ({ onClick }) => {
+const LoginForm: React.FC<Props> = ({onClick}) => {
 
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: '',
+  })
 
-    return (
-        <form className='form-login'>
-            <img className='form-login__logo' src={require('../../images/logo-mk.png')} alt='' width='124' height='76' />
-            <label className='form-login__label'>
-                <input className='form-login__input'
-                    type='text'
-                    id='email'
-                    placeholder={labels.form.email}
-                >
-                </input>  
-            </label>
-                          
-            <label className='form-login__label'>
-                <input className='form-login__input'
-                    type='text'
-                    id='password'
-                    placeholder={labels.form.password}
-                >
-                </input>
-            </label>
-            
-            <div className='form-login__div'>
-                <div className='form-login__login'>
-                    <ButtonLink type='submit'
-                        customClass='red-btn'
-                        label={labels.buttons.login}
-                    />
-                </div>
-                <div className='form-login__forgotten-password'>
-                    <ButtonLink type='button'
-                        label={labels.form.forgottenPassword}
-                        onClick={onClick}
-                    />
-                </div>
-           </div>
-        </form>
-    )
+  const handleChange = (key: string, value: any) => {
+    setLoginForm(dataItem => ({
+      ...dataItem,
+      [key]: value
+    }))
+  }
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        body: JSON.stringify(loginForm),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await res.json()
+
+      if (!data.success) throw new Error('Something went wrong.')
+
+      switch (data.role){
+        case 'admin':
+          window.location.href = '/admin'
+          break;
+        case 'student':
+          window.location.href = '/student'
+          break;
+        case 'recruiter':
+          window.location.href = '/recruiter'
+          break;
+      }
+
+    } catch (e){
+      console.log(e)
+    }
+
+  }
+
+  return (
+    <form className='form-login' onSubmit={handleSubmit}>
+      <img className='form-login__logo' src={require('../../images/logo-mk.png')} alt='' width='124' height='76'/>
+      <label className='form-login__label'>
+        <input className='form-login__input'
+               type='text'
+               id='email'
+               placeholder={labels.form.email}
+               value={loginForm.email}
+               onChange={e => handleChange('email', e.target.value)}
+        >
+        </input>
+      </label>
+
+      <label className='form-login__label'>
+        <input className='form-login__input'
+               type='text'
+               id='password'
+               placeholder={labels.form.password}
+               value={loginForm.password}
+               onChange={e => handleChange('password', e.target.value)}
+        >
+        </input>
+      </label>
+
+      <div className='form-login__div'>
+        <div className='form-login__login'>
+          <ButtonLink type='submit'
+                      customClass='red-btn'
+                      label={labels.buttons.login}
+          />
+        </div>
+        <div className='form-login__forgotten-password'>
+          <ButtonLink type='button'
+                      label={labels.form.forgottenPassword}
+                      onClick={onClick}
+          />
+        </div>
+      </div>
+    </form>
+  )
 }
 
 export default LoginForm;
